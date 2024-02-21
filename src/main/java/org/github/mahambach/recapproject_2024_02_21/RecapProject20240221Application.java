3ad8b1473e233a819c1f5419769a85b1f79a896b
mahambach <ma.hambach@gmx.net>
2024-02-21 14:16:48 +0100

    Initial commit

---
+++ src/main/java/org/github/mahambach/recapproject_2024_02_21/RecapProject20240221Application.java
@@ -0,0 +1,13 @@
+package org.github.mahambach.recapproject_2024_02_21;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecapProject20240221Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecapProject20240221Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/github/mahambach/recapproject_2024_02_21/RecapProject20240221ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.github.mahambach.recapproject_2024_02_21;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecapProject20240221ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
